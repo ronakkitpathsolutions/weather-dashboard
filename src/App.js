175273f30f4690a70b815254b14cede054a202ca
Ronak Kapadi <ronakk@itpathsolutions.com>
2024-06-08 11:42:06 +0530

    Install husky eslint and prettier

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <img
+      alt="profile"
+      src="http://192.168.0.21:8077/Image/9ba51e15-646e-479c-b071-56c60f145e88"
+    />
+  )
 }
 
-export default App;
+export default App
