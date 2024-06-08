bae44414b8496c8a550f90e3efc1f9dcaaa65521
Ronak Kapadi <ronakk@itpathsolutions.com>
2024-06-08 12:11:22 +0530

    FIX: eslint+husky+prettier setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+	return (
+		<img
+			alt="profile"
+			src="http://192.168.0.21:8077/Image/9ba51e15-646e-479c-b071-56c60f145e88"
+		/>
+	);
+}
+
+export default App;
