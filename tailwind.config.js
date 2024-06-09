75fff229aef5d8b9d1aa21addfda835404dca83d
Ronak Kapadi <ronakkapadi@Ronaks-MacBook-Air.local>
2024-06-09 17:36:10 +0530

    Update config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 module.exports = {
 	content: ['./src/**/*.{js,jsx,ts,tsx,html}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				transparent: 'transparent',
+				current: 'currentColor',
+				white: '#ffffff',
+				black: '#000000',
+				primary: {},
+				secondary: {
+					100: '#F8F9FA',
+					200: '#E9ECEF',
+					300: '#DEE2E6',
+					400: '#CED4DA',
+					500: '#ADB5BD',
+					600: '#6C757D',
+					700: '#495057',
+					800: '#343A40',
+					900: '#212529',
+				},
+			},
+		},
 	},
 	plugins: [],
 }
