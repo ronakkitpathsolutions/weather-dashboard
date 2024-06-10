f1b7f1f1a4ad78c89c28d64d0e91515ee5694001
Ronak Kapadi <ronakk@itpathsolutions.com>
2024-06-10 11:10:33 +0530

    Updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,30 @@ module.exports = {
 	content: ['./src/**/*.{js,jsx,ts,tsx,html}'],
 	theme: {
 		extend: {
+			screens: {
+				'2xs': '280px',
+				xs: '375px',
+			},
 			colors: {
 				transparent: 'transparent',
 				current: 'currentColor',
 				white: '#ffffff',
 				black: '#000000',
-				primary: {},
+				primary: {
+					100: '#fffbf8',
+					200: '#f0e7df',
+					300: '#e5d9ce',
+					400: '#af9d8c',
+					500: '#8b7e71',
+				},
 				secondary: {
+					100: '#9ba288',
+					200: '#8b9373',
+					300: '#6f7757',
+					400: '#5c6349',
+					500: '#535942',
+				},
+				initial: {
 					100: '#F8F9FA',
 					200: '#E9ECEF',
 					300: '#DEE2E6',
